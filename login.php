b89f98914fd48e6fef233f2906566cc95f312cf5
Celia-code <infinite010750@gmail.com>
2022-02-19 17:00:12 +0800

    change all file's html head area to header.php

--- login.php
+++ login.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-
-</head>
+<?php include_once"header.php";?>
 <body>
     <div class="wrapper">
         <section class="form login">
